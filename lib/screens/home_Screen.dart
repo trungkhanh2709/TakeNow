92e3bdd95b8387074d177fdbba9f0af4260a6ed1
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2024-06-24 17:36:01 +0700

    Update home_Screen.dart
    test

--- lib/screens/home_Screen.dart
+++ lib/screens/home_Screen.dart
@@ -15,11 +15,15 @@ class _HomeScreenState extends State<HomeScreen> {
   late List<CameraDescription> cameras;
   late CameraController _controller;
   bool _isCameraInitialized = false;
+<<<<<<< Updated upstream
   int _currentCameraIndex = 0;
   double _maxZoom = 1.0;
   double _minZoom = 1.0;
   double _currentZoom = 1.0;
   double _zoomSpeedMutiplier = 0.008;
+=======
+  int _currentCameraIndex = 0; // Index to track current camera
+>>>>>>> Stashed changes
 
   @override
   void initState() {
@@ -45,6 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     try {
       await _controller.initialize();
+<<<<<<< Updated upstream
       double zoom = await _controller.getMaxZoomLevel();
 
       setState(() {
@@ -52,6 +57,10 @@ class _HomeScreenState extends State<HomeScreen> {
         _maxZoom= zoom;
         _minZoom = 1.0;
         _currentZoom = 1.0;
+=======
+      setState(() {
+        _isCameraInitialized = true;
+>>>>>>> Stashed changes
       });
     } catch (e) {
       print('Error initializing camera: $e');
@@ -72,7 +81,13 @@ class _HomeScreenState extends State<HomeScreen> {
     }
 
     double screenWidth = MediaQuery.of(context).size.width;
+<<<<<<< Updated upstream
     double cameraPreviewSize = screenWidth * 0.95;
+=======
+
+    // Calculate the size for the CameraPreview to maintain 1:1 aspect ratio
+    double cameraPreviewSize = screenWidth * 0.9;
+>>>>>>> Stashed changes
 
     return Scaffold(
       appBar: AppBar(
@@ -82,12 +97,17 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: () {
               _onSwitchCamera();
             },
+<<<<<<< Updated upstream
             icon: Icon(Icons.person),
+=======
+            icon: Icon(Icons.switch_camera),
+>>>>>>> Stashed changes
           ),
           IconButton(
             onPressed: () {
               _onToggleFlash();
             },
+<<<<<<< Updated upstream
             icon: Icon(Icons.chat_bubble_outline_outlined),
           ),
         ],
@@ -130,15 +150,40 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: CameraPreview(_controller),
                     ),
                   ),
+=======
+            icon: Icon(Icons.flash_on),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          width: cameraPreviewSize,
+          height: cameraPreviewSize,
+          child: ClipRect(
+
+            child: OverflowBox(
+              alignment: Alignment.center,
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: SizedBox(
+                  width: _controller.value.previewSize!.height,
+                  height: _controller.value.previewSize!.width,
+                  child: CameraPreview(_controller),
+>>>>>>> Stashed changes
                 ),
               ),
             ),
           ),
+<<<<<<< Updated upstream
           ),
         ],
 
         ),
 
+=======
+        ),
+      ),
+>>>>>>> Stashed changes
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -161,7 +206,10 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
   void _onSwitchCamera() {
     _currentCameraIndex = (_currentCameraIndex + 1) % cameras.length;
     _controller = CameraController(
@@ -174,6 +222,7 @@ class _HomeScreenState extends State<HomeScreen> {
         setState(() {});
       }
     });
+<<<<<<< Updated upstream
 
     _controller.initialize().then((_) {
       setState(() {});
@@ -215,7 +264,37 @@ class _HomeScreenState extends State<HomeScreen> {
     double y = details.localPosition.dx / MediaQuery.of(context).size.height;
     Offset focusPoint = Offset(x, y); // Create Offset object
     _controller.setFocusPoint(focusPoint);
+=======
+>>>>>>> Stashed changes
 
+    _controller.initialize().then((_) {
+      setState(() {});
+    });
+  }
 
+  void _onToggleFlash() {
+    if (_controller.value.isInitialized) {
+      bool currentFlashMode = _controller.value.flashMode == FlashMode.torch;
+      _controller.setFlashMode(currentFlashMode ? FlashMode.off : FlashMode.torch);
+    }
+  }
+
+  void _onCapturePressed() async {
+    try {
+      XFile file = await _controller.takePicture();
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Scaffold(
+            appBar: AppBar(),
+            body: Center(
+              child: Image.file(File(file.path)),
+            ),
+          ),
+        ),
+      );
+    } catch (e) {
+      print('Error taking picture: $e');
+    }
   }
 }
