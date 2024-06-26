bf3fd9f342ca9f3a7fb7be87a579ccfa298c6ef5
trungkhanh2709 <111423000+trungkhanh2709@users.noreply.github.com>
2024-06-27 03:55:24 +0700

    viewPhotoScreen.dart

---
+++ lib/screens/viewPhotoScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewPhotoScreen extends StatefulWidget {
+  const ViewPhotoScreen({super.key});
+
+  @override
+  State<ViewPhotoScreen> createState() => _ViewPhotoScreenState();
+}
+
+class _ViewPhotoScreenState extends State<ViewPhotoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
